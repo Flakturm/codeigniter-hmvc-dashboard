d5f313c1861a33e14a710b54de64e2356be89a00
RedOctober <admin@andy-web-dev.com>
2016-07-13 18:50:43 +1200

    remove link

--- application/views/front/bank_note.php
+++ application/views/front/bank_note.php
@@ -5,7 +5,7 @@
 			<span class="h2 warnings-title">謹慎理財 信用至上</span>
 		</div>
 		<div class="col-md-8 col-sm-7 col-xs-12 warnings-text">
-		信用卡循環年利率：本行ARMs指數+加碼利率(5.97%~13.47%)；上限為15%，預借現金手續費為每筆預借金額X3.5%+150元，循環利率基準日為104年9月1日，其他費用請上 <a href="https://www.ctbcbank.com/html/announce/financialinfo/content_931129_popup_2.html" target="_blank">官網</a> 查詢。
+		信用卡循環年利率：本行ARMs指數+加碼利率(5.97%~13.47%)；上限為15%，預借現金手續費為每筆預借金額X3.5%+150元，循環利率基準日為104年9月1日，其他費用請上 <a href="" target="_blank">官網</a> 查詢。
 		</div>
 	</div>
 </div>
